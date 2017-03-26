f728b916f6b13113a6dabad7117c534249d60383
aishwaryau19 <aishwaryau19@gmail.com>
2017-03-26 02:05:32 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Profile = require('../models/profile');
+var Expense = require('../models/expense');
 
 
 /* GET home page. */
@@ -43,13 +44,11 @@ router.post('/updateprofile', isLoggedIn, function(req,res,next){
         feessix:req.body.fess6
     });
 
-    var userID = req.user.id;
-
-
+        var userID = req.user.id;
         Profile.find({'user':userID}, function (err, todo) { 
         if (err) {
         res.status(500).send(err);
-    } else {
+        } else {
         Profile.update({'user':userID},{
         expenditure:req.body.expenditure,
         income:req.body.monthincome,
@@ -59,19 +58,43 @@ router.post('/updateprofile', isLoggedIn, function(req,res,next){
             console.log(res);
             res.redirect('http://google.com');
         });
-/*
-Updateprofile.update(function (err, todo) {
-            if (err) {
-                res.status(500).send(err)
-            }
-            res.send(Updateprofile);
-        });*/
     
-}
-     });
-
-
+        }
+    });
 });
+
+     //Expense
+router.post('/expenseEntry', isLoggedIn, function(req,res,next){
+    console.log(req.body);
+    var expenseProfile = new Expense({
+        user:req.user,
+        ExpenseType:req.body.ExpenseType,
+        ExpenseAmount:req.body.ExpenseAmount,
+        comments:req.body.comments
+    });
+    console.log(expenseProfile);
+    expenseProfile.save(function(err,result){
+            req.flash('success','Successfully bought the product');
+            req.session.cart = null;
+            res.redirect('/');
+            //res.redirect('/');    
+        });
+});
+
+
+
+router.get('/update/',function(req,res,next){
+    res.render('user/update');
+});
+
+router.get('/expense/',function(req,res,next){
+    res.render('user/expense');
+});
+
+/*
+router.get('/expense/',function(req,res,next){
+    res.render('user/expense');
+});*/
 
 module.exports = router;
 
