12bfae166f1f7b1c3de1263c783ed073cf1b22d2
aishwaryau19 <aishwaryau19@gmail.com>
2017-03-26 00:39:17 -0400

    Add files via upload

--- models/profile.js
+++ models/profile.js
@@ -4,8 +4,11 @@ var Schema = mongoose.Schema;
 var schema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: 'User'},
     expenditure:{type: String,required:true},
+    expenditure2:{type: String},
+    expenditure3:{type: String},
+    expenditure4:{type: String},
+    expenditure5:{type: String},        
     income:{type: String,required:true},
-    savingsMin:{type: String,required:true},
     feessix:{type: String,required:true}
 });
 
