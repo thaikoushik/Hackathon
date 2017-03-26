8f0de98a1e5768074e4421bbc48e3124967f2787
aishwaryau19 <aishwaryau19@gmail.com>
2017-03-26 00:41:43 -0400

    Add files via upload

---
+++ public/javascripts/util.js
@@ -0,0 +1,14 @@
+$(document).ready(function(e){
+
+    $( document ).on( 'click', '.bs-dropdown-to-select-group .dropdown-menu li', function( event ) {
+        var $target = $( event.currentTarget );
+        $target.closest('.bs-dropdown-to-select-group')
+            .find('[data-bind="bs-drp-sel-value"]').val($target.attr('data-value'))
+            .end()
+            .children('.dropdown-toggle').dropdown('toggle');
+        $target.closest('.bs-dropdown-to-select-group')
+            .find('[data-bind="bs-drp-sel-label"]').text($target.context.textContent);
+        return false;
+    });
+
+});
